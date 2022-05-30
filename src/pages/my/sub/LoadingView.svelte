b912d5e2531c4a93e82665a0c3d79f90c36ee718
oouuyab <oouuyab@gmail.com>
2022-05-30 23:58:32 +0900

    api 레이턴시가 길어 로딩 뷰 추가

---
+++ src/pages/my/sub/LoadingView.svelte
@@ -0,0 +1,51 @@
+<script></script>
+
+<article>
+  <div>
+    <h1>분석중...</h1>
+    <h2>병용금기 사항이 있는지 알아보고 있어요</h2>
+    <div class='img-wrapper'>
+      <img src='/assets/gif/pill.gif' alt='loading' />
+    </div>
+  </div>
+</article>
+
+<style lang='scss'>
+  article {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height :100vh;
+    background-color: #fff;
+    div {
+      width: 100%;
+      margin-top: 32px;
+      h1 {
+        color: #2C73D2;
+        margin-left: 26px;
+        font-size: 26px;
+        margin-bottom: 8px;
+      }
+      h2 {
+        color: rgba(64,64,64,1);
+        font-size: 18px;
+        margin-left: 26px;
+        margin-bottom: 120px;
+      }
+      div.img-wrapper {
+        position: fixed;
+        bottom: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        img {
+          border-radius: 200px;
+          width: 80vw;
+        }
+      }
+    }
+  }
+</style>
