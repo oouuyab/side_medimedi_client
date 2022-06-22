e35d37e12b47bfbd7080affb7581e6aab63ffd45
oouuyab <oouuyab@gmail.com>
2022-06-22 16:21:16 +0900

    axios url에 host 제거

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 export const search = async (keyword) =>
   await axios({
     method: 'GET',
-    url: SERVER_HOST + `/medi/search?keyword=${keyword}`,
+    url: `/api/medi/search?keyword=${keyword}`,
   });
 
 export const searchDURInfo = async (params) =>
   await axios({
     method: 'POST',
-    url: SERVER_HOST + `/medi/search/dur`,
+    url: '/api/medi/search/dur',
     data: params,
   });
